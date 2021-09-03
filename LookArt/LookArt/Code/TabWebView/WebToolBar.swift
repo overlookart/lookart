8e0d708294acea0ed32e64f671768d6faa57bd74
xzh <>
2021-09-03 11:45:54 +0800

    新增工具栏

---
+++ LookArt/LookArt/Code/TabWebView/WebToolBar.swift
@@ -0,0 +1,58 @@
+//
+//  WebToolBar.swift
+//  LookArt
+//
+//  Created by CaiGou on 2021/9/3.
+//
+
+import UIKit
+
+class WebToolBar: UIToolbar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    /// 后退
+    private(set) var backBtnItem = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: nil, action: nil)
+    /// 前进
+    private(set) var forwardBtnItem = UIBarButtonItem(image: UIImage(systemName: "chevron.right"), style: .plain, target: nil, action: nil)
+    
+    private(set) var actionBtnItem = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: nil, action: nil)
+    /// 书签
+    private(set) var bookmarkBtnItem = UIBarButtonItem(image: UIImage(systemName: "book"), style: .plain, target: nil, action: nil)
+    /// 标签
+    private(set) var tabmarkBtnItem = UIBarButtonItem(image: UIImage(systemName: "square.on.square"), style: .plain, target: nil, action: nil)
+    
+    private var allItems: [UIBarButtonItem]!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("创建 工具栏",self);
+//        self.setItems(items, animated: false)
+        self.allItems = [self.backBtnItem, createFlexibleSpaceItem(), self.forwardBtnItem, createFlexibleSpaceItem(), self.actionBtnItem, createFlexibleSpaceItem(), self.bookmarkBtnItem, createFlexibleSpaceItem(), self.tabmarkBtnItem]
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// 创建填充item之间的item
+    /// - Returns: item
+    private func createFlexibleSpaceItem() -> UIBarButtonItem {
+        return UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    }
+    override var items: [UIBarButtonItem]? {
+        get{
+            return self.allItems
+        }
+        set{
+            super.items = newValue
+        }
+    }
+}
