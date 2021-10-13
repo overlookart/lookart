3fc9ecfbb5d6fe41f3aef43aae16fb181a757b93
xzh <>
2021-10-13 18:42:05 +0800

    设置选中状态

--- LookArt/LookArt/Code/Theme/ThemeViewCell.swift
+++ LookArt/LookArt/Code/Theme/ThemeViewCell.swift
@@ -12,11 +12,26 @@ class ThemeViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        //右侧附件类型
+        //    .none 啥也没有
+        //    .checkmark 蓝色对勾
+        //    .disclosureIndicator 灰色右箭头
+        //    .detailDisclosureButton 蓝色圆圈i + 灰色右箭头
+        //    .detailButton 蓝色圆圈i
+//        self.accessoryType = .checkmark
     }
-
+    
+    /// 设置选中状态
+    /// - Parameters:
+    ///   - selected: 选中状态
+    ///   - animated: 是否有动画
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        if selected {
+            self.accessoryType = .checkmark
+        }else{
+            self.accessoryType = .none
+        }
         // Configure the view for the selected state
     }
     
