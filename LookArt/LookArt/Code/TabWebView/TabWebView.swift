ef3412b9555a35e6b8038a5e69e7c1395154d608
xzh <18334786772@163.com>
2024-07-03 15:20:57 +0800

    因加载页面白屏， webview 自身将不再遵守 UIScrollViewDelegate，

--- LookArt/LookArt/Code/TabWebView/TabWebView.swift
+++ LookArt/LookArt/Code/TabWebView/TabWebView.swift
@@ -19,14 +19,6 @@ class TabWebView: BaseWebView {
     private(set) var scrollBeginDragOffset: CGPoint = CGPoint.zero
     /// 当前的offset
     private(set) var scrollContentOffset: CGPoint = CGPoint.zero
-    /// 滑动代理
-    var scrollDelegates: (
-        DidScroll:(() -> Void)?,
-        BeginDragging:(() -> Void)?,
-        WillEndDragging:((CGPoint) -> Void)?,
-        EndDragging:(() -> Void)?,
-        BeginDecelerating:(() -> Void)?,
-        EndDecelerating:(() -> Void)?)?
     
     /// web导航代理毁掉
     var navigationDelegates: (
@@ -83,48 +75,6 @@ class TabWebView: BaseWebView {
     }
     deinit {
         NotificationCenter.default.removeObserver(self)
-    }
-}
-
-extension TabWebView: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        self.scrollContentOffset = scrollView.contentOffset
-        if let didScroll = self.scrollDelegates?.DidScroll {
-            didScroll()
-        }
-    }
-    
-    /// 开始滑动
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        self.scrollBeginDragOffset = scrollView.contentOffset
-        if let beginDragging = self.scrollDelegates?.BeginDragging {
-            beginDragging()
-        }
-    }
-    ///将要结束拖拽
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        if let willEndDragging = self.scrollDelegates?.WillEndDragging {
-            willEndDragging(velocity)
-        }
-    }
-    
-    /// 结束拖拽
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if let endDragging = self.scrollDelegates?.EndDragging {
-            endDragging()
-        }
-    }
-    /// 开始减速
-    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
-        if let beginDecelerating = self.scrollDelegates?.BeginDecelerating {
-            beginDecelerating()
-        }
-    }
-    /// 停止减速
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        if let endDecelerating = self.scrollDelegates?.EndDecelerating {
-            endDecelerating()
-        }
     }
 }
 
